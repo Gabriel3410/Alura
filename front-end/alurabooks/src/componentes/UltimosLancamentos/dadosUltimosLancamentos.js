cbb54db4ea043c1942ed34f95f0134405cbda399
gabriel soares <gabrielsantossoares18@gmail.com>
2024-07-22 18:58:35 -0300

    React

---
+++ front-end/alurabooks/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id:1, src:livro},
+    {nome: 'Cangaceiro JavaScript', id:2, src:livro},
+    {nome: 'Guia do PHP', id:3, src:livro},
+]
